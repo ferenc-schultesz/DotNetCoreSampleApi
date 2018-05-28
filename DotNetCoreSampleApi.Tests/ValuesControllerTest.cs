4624fdb9487dce7cbe3ceac4d059a9bbf5bb6fbd
Ferenc Schultesz <ferenc.schultesz@hackney.gov.uk>
2018-05-28 23:58:22 +0100

    Initial commit

---
+++ DotNetCoreSampleApi.Tests/ValuesControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using DotNetCoreSampleApi.Controllers;
+using Xunit;
+
+namespace DotNetCoreSampleApi.Tests
+{
+	public class ValuesControllerTest
+    {
+        [Fact]
+		public void Get_Should_Return_Value1_And_Value2()
+        {
+			var controller = new ValuesController();
+			var values = controller.Get().ToList();
+
+			Assert.True(values.Contains("value1"), "value1 is not returned");
+            Assert.True(values.Contains("value2"), "value2 is not returned");
+        }
+    }
+}
